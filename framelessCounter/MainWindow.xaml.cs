a48d2a7e1d82f0d12efa10655262a86211de3a39
Ben <ben399399@naver.com>
2018-03-27 09:04:23 +0900

    Added buttons
    rewind, play, close

--- framelessCounter/MainWindow.xaml.cs
+++ framelessCounter/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 
+
+
 namespace framelessCounter
 {
     /// <summary>
@@ -21,27 +23,39 @@ namespace framelessCounter
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const int targetTime = 20;
+        public bool isplaying = false;
         public DispatcherTimer Timer = new DispatcherTimer();
-        DateTime startTime = DateTime.Now;
+        private DateTime startTime;
+        private bool onlyOnce = true;
         public MainWindow()
         {
             InitializeComponent();
-            Timer.Interval = TimeSpan.FromSeconds(1);
-            Timer.Tick += new EventHandler(Timer_Tick);
-            Timer.Start();
+            clockText.Content = "20 : 00";
+        }
+
+        ~MainWindow()
+        {
+            Timer.Stop();
         }
 
         private void Timer_Tick(object sender, EventArgs e)
         {
             DateTime currentTime = DateTime.Now;
             TimeSpan diff = currentTime.Subtract(startTime);
-            if ((1 - diff.Minutes) >= 0)
-            {
-            }
-            else { 
+            if (((targetTime - 1) - diff.Minutes) < 0)
                 clockText.Foreground = new SolidColorBrush(Colors.Red);
-            }
-            clockText.Content = ((1 - diff.Minutes)>=0? (1 - diff.Minutes).ToString() :"+"+(diff.Minutes-2)) + " : " + ((1 - diff.Minutes) >= 0 ? (60 - diff.Seconds):diff.Seconds);
+            clockText.Content = (((targetTime-1) - diff.Minutes)>=0
+                                    ? ((targetTime - 1) - diff.Minutes).ToString() 
+                                    :"+"+(diff.Minutes- (targetTime + 1))) 
+                                + " : " + 
+                                (((targetTime - 1) - diff.Minutes) < 0 
+                                    ? diff.Seconds<10
+                                      ? "0" + diff.Seconds
+                                      : diff.Seconds.ToString()
+                                    : (59- diff.Seconds)<10
+                                        ? "0"+ (59 - diff.Seconds) 
+                                        : (59 - diff.Seconds).ToString());
         }
 
         private void Window_Deactivated(object sender, EventArgs e)
@@ -49,6 +63,34 @@ namespace framelessCounter
             Window window = (Window)sender;
             window.Topmost = true;
         }
-        
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Button_Click_play(object sender, RoutedEventArgs e)
+        {
+
+            if (!isplaying)
+            {
+                //now starting 
+                startTime = DateTime.Now;
+                if (onlyOnce)
+                {
+                    Timer.Interval = TimeSpan.FromSeconds(0.5);
+                    Timer.Tick += new EventHandler(Timer_Tick);
+                    onlyOnce = false;
+                }
+                Timer.Start();
+            }
+            isplaying = !isplaying;
+        }
+        private void resetButton_Click(object sender, RoutedEventArgs e)
+        {
+            startTime = DateTime.Now;
+            Timer.Stop();
+            clockText.Content = "20 : 00";
+        }
     }
 }
